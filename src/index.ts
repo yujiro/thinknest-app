04ed4ee0d878f09541aa9cefc2c1136d8019c2f9
Yujiro Arai <yujiroarai@gmail.com>
2023-05-14 12:19:16 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 import { app, BrowserWindow, ipcMain, shell } from 'electron'
 import config from './config'
-import path from 'path'
-import { Deeplink } from 'electron-deeplink'
-import electronIsDev from 'electron-is-dev'
 
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit()
 }
@@ -37,29 +33,10 @@ const createWindow = (): void => {
       )
     }
   })
-
-  // new Deeplink({ app, mainWindow, protocol: config().scheme, isDev: electronIsDev });
-
-  // if (process.defaultApp) {
-  //   if (process.argv.length >= 2) {
-  //     app.setAsDefaultProtocolClient(config().scheme, process.execPath, [
-  //       path.resolve(process.argv[1]),
-  //     ])
-  //   }
-  // } else {
-  //   app.setAsDefaultProtocolClient(config().scheme)
-  // }
-  
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
@@ -67,15 +44,10 @@ app.on('window-all-closed', () => {
 })
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
 
 ipcMain.handle('openLogin', (event, arg) => {
   shell.openExternal(`${config().appUrl}/appLogin`)
